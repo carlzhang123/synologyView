21497598284e898814928ba26b8f930bc86b73f8
carlzhang123 <carl.dev@carlzhang.xyz>
2026-09-04 12:02:59 +0800

    Initial Commit

---
+++ SynologyView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SynologyView
+//
+//  Created by 张杰 on 2026/9/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SynologyView/SynologyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynologyViewApp.swift
+//  SynologyView
+//
+//  Created by 张杰 on 2026/9/4.
+//
+
+import SwiftUI
+
+@main
+struct SynologyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
